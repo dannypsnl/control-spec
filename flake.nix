da8bac21ba0ab850b3cbace016a71ac2b36e4b95
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-12-28 12:02:44 +0800

    nix flakes init
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "unit test framework for idris2";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    idris-lang.url = "github:idris-lang/Idris2";
+    idris-lang.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, idris-lang }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        stdenv = pkgs.stdenv;
+
+        idris2 = idris-lang.packages.${system}.idris2;
+        rlwrap = pkgs.rlwrap;
+
+        control-spec = stdenv.mkDerivation {
+          name = "control-spec";
+          version = "0.0.0";
+          src = ./.;
+
+          buildInputs = [ idris2 ];
+          buildPhase = ''
+            idris2 --build ./control-spec.ipkg
+          '';
+          installPhase = ''
+            export HOME=$(pwd)
+            mkdir -p $out/bin
+            mv ./build/exec/* $out/bin/
+          '';
+        };
+      in rec {
+        packages = { control-spec = control-spec; };
+        packages.default = packages.control-spec;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ idris2 rlwrap control-spec ];
+        };
+      }
+    );
+}
